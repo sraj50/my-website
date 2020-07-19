bc764237462626c9a6fb7ef296573d564abbb613
Sid Raj <siddharth.rajesh13@gmail.com>
2020-07-19 15:17:12 +1000

    a simple query

--- src/index.js
+++ src/index.js
@@ -2,15 +2,34 @@ const { GraphQLServer } = require('graphql-yoga')
 
 // define GraphQL schema
 const typeDefs = `
-type Query {
-  info: String!
-}
+    type Query {
+        info: String!
+        feed: [Link!]!
+    }
+
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
+    }
 `
+// dummy data
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
 
 // implement GraphQL schema with resolvers
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Link: {
+      id: (parent) => parent.id,
+      description: (parent) => parent.description,
+      url: (parent) => parent.url
   }
 }
 
