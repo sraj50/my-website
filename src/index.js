24b567ca9cd699e0867a1901f407c284c52bb8a7
Sid Raj <siddharth.rajesh13@gmail.com>
2020-07-19 20:12:30 +1000

    a simple mutation

--- src/index.js
+++ src/index.js
@@ -1,41 +1,59 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } = require("graphql-yoga")
 
-// define GraphQL schema
-const typeDefs = `
-    type Query {
-        info: String!
-        feed: [Link!]!
-    }
+// const typeDefs = `
+//  type Query {
+//    info: String!
+//    feed: [Link!]!
+//   }
 
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-    }
-`
+//   type Mutation {
+//     post(url: String!, description: String!): Link!
+//   }
+
+//   type Link {
+//    id: ID!
+//    description: String!
+//    url: String!
+//   }
+// `
+
 // dummy data
 let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+}, ];
+
+let idCount = links.length;
 
 // implement GraphQL schema with resolvers
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
-  },
-  Link: {
-      id: (parent) => parent.id,
-      description: (parent) => parent.description,
-      url: (parent) => parent.url
-  }
-}
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+    },
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
+            }
+            links.push(link)
+            return link
+        }
+    },
+    // Link: {
+    //   id: (parent) => parent.id,
+    //   description: (parent) => parent.description,
+    //   url: (parent) => parent.url,
+    // },
+};
 
 // GraphQL server
 const server = new GraphQLServer({
-  typeDefs,
-  resolvers,
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+    typeDefs: './src/schema.graphql',
+    // typeDefs,
+    resolvers,
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
